e89ec16c3b65d71fcbe14af87d06868e7a68b73a
Julie Klimentova <julie.klimentova@gmail.com>
2020-09-20 12:29:21 +0200

    DATA COLLECTOR - Genius Client working fully

--- DataCollector/DataCollector.js
+++ DataCollector/DataCollector.js
@@ -1,5 +1,8 @@
 import {WikipediaClient} from './WikipediaClient';
+import {GeniusClient} from './GeniusClient';
 import * as Helpers from './Helpers';
+import fs from 'fs';
+import ObjectsToCsv from 'objects-to-csv';
 
 const wikiOptions = {
     apiUrl: 'https://fr.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=',
@@ -15,6 +18,7 @@ const wikiOptions = {
 
 const wordsToClean = [
     '(rappeur)',
+    '(rappeuse)',
     '(musicien)',
     '(rap)',
     '(chanteuse)',
@@ -23,15 +27,21 @@ const wordsToClean = [
     '(collectif)',
     '(groupe de rap)',
     '(artiste)',
-    '(rappeur français)'
+    '(rappeur français)',
+    '(groupe belge)'
 ];
 
+const geniusBaseUrl = 'https://api.genius.com';
+
 class DataCollector {
-    _wikiPediaClient;
     constructor(wikiOptions, wordsToClean) {
         this._wikiPediaClient = new WikipediaClient();
+        this._geniusClient = new GeniusClient(geniusBaseUrl);
         this._wikiOptions = wikiOptions;
         this._wordsToClean = wordsToClean;
+        fs.mkdir('./files', (e) => {
+            console.log(e);
+        })
     }
 
     async getAllArtists() {
@@ -48,10 +58,76 @@ class DataCollector {
         return Helpers.removeDuplicates(artistsTrimmed);
     }
 
+    async getAllArtistsIds(artists) {
+        const artistsIds = [];
+        for (const artist of artists) {
+            const artistInfo = await this._geniusClient.getArtistId(artist);
+            if (artistInfo) {
+                const {artistId} = artistInfo;
+                if (artistId && artistId !== '') {
+                    artistsIds.push(artistInfo);
+                }
+            } else {
+                console.log(`DataCollector: ERROR: Artist Id was not found for artist ${artist}`);
+            }
+        }
+        return artistsIds;
+    }
+
+    async getSongsPerArtist(getSongsArtistInfo) {
+        const songs = await this._geniusClient.getSongs(getSongsArtistInfo);
+        return {...getSongsArtistInfo, songs};
+    }
+
+    async getSongsTexts(textArtistInfo) {
+        return this._geniusClient.getSongsTexts(textArtistInfo);
+    }
+    writeToTxt(song) {
+        if (!song.lyrics.includes('no lyrics found')) {
+            try {
+                fs.writeFileSync(`./files/${song.txtName}.txt`, song.lyrics);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    }
+    async writeToCsv(objects) {
+        const csv = new ObjectsToCsv(objects);
+        await csv.toDisk('./songsMetadata.csv', {append: true});
+    }
 }
 
 const dataCollector = new DataCollector(wikiOptions, wordsToClean);
 
-dataCollector.getAllArtists()
-    .then(results => console.log(results))
-    .catch(e => console.log(e));
+const main = async () => {
+    const artistsNames = await dataCollector.getAllArtists();
+    console.log('MAIN: Artists names have been received');
+    const artistsInfos = await dataCollector.getAllArtistsIds(artistsNames);
+    console.log('MAIN: Artists infos have been received');
+    const artistsWithSongs = [];
+    for (const artistInfo of artistsInfos) {
+            const artistWithSong = await dataCollector.getSongsPerArtist(artistInfo);
+            artistsWithSongs.push(artistWithSong);
+    }
+    console.log('MAIN: Artists with songs have been retrieved' + JSON.stringify(artistsWithSongs));
+    const allSongs = []
+    for (const artistWithSong of artistsWithSongs) {
+        const songs = await dataCollector.getSongsTexts(artistWithSong);
+        songs.forEach((song) => {
+            allSongs.push(song);
+            dataCollector.writeToTxt(song);
+        });
+    }
+    console.log('MAIN: All songs have been retrieved' + JSON.stringify(allSongs));
+    try {
+        fs.writeFileSync('./allSongs.json', JSON.stringify(allSongs));
+    } catch (e) {
+        console.log(e);
+    }
+    await dataCollector.writeToCsv(allSongs);
+}
+
+main()
+    .then(() => console.log('All done, captain!'))
+        .catch((e) => console.log(e));
+
