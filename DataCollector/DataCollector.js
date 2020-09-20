5710317224d56c71058cc2b3eb05d4eb490ee77d
Julie Klimentova <julie.klimentova@gmail.com>
2020-09-20 09:27:34 +0200

    DATA COLLECTOR - Cleaning up wiki data

--- DataCollector/DataCollector.js
+++ DataCollector/DataCollector.js
@@ -1,21 +1,57 @@
 import {WikipediaClient} from './WikipediaClient';
+import * as Helpers from './Helpers';
 
+const wikiOptions = {
+    apiUrl: 'https://fr.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=',
+    categories: [
+        'Cat%C3%A9gorie:Rappeur_fran%C3%A7ais',
+        'Cat%C3%A9gorie%3ARappeur_belge',
+        'Cat%C3%A9gorie%3AGroupe_de_hip-hop_fran%C3%A7ais',
+        'Cat%C3%A9gorie%3AGroupe_de_hip-hop_belge',
+        'Cat%C3%A9gorie%3ARappeuse_belge',
+        'Cat%C3%A9gorie%3ARappeuse_fran%C3%A7aise'
+    ],
+}
 
+const wordsToClean = [
+    '(rappeur)',
+    '(musicien)',
+    '(rap)',
+    '(chanteuse)',
+    '(chanteur)',
+    '(groupe)',
+    '(collectif)',
+    '(groupe de rap)',
+    '(artiste)',
+    '(rappeur français)'
+];
 
 class DataCollector {
     _wikiPediaClient;
-    constructor() {
+    constructor(wikiOptions, wordsToClean) {
         this._wikiPediaClient = new WikipediaClient();
+        this._wikiOptions = wikiOptions;
+        this._wordsToClean = wordsToClean;
     }
 
-    async getAllCategories() {
-        const categoriesRappers = await this._wikiPediaClient.getCategories('https://fr.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=', 'Cat%C3%A9gorie:Rappeur_fran%C3%A7ais');
-        console.log(JSON.stringify(categories));
-        const categoriesGroups = await this._wikiPediaClient.getCategories('https://fr.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=', 'Cat%C3%A9gorie%3AGroupe_de_hip-hop_fran%C3%A7ais');
-        console.log(JSON.stringify(categoriesGroups));
+    async getAllArtists() {
+        const artists = [];
+        const {apiUrl, categories} = this._wikiOptions;
+        for (const category of categories) {
+            const result = await this._wikiPediaClient.getArtists(apiUrl, category);
+            const categoryMembers = result.query.categorymembers;
+            for (const entry of categoryMembers) {
+                artists.push(entry.title);
+            }
+        }
+        const artistsTrimmed = Helpers.cleanStrings(this._wordsToClean, artists);
+        return Helpers.removeDuplicates(artistsTrimmed);
     }
+
 }
 
-const dataCollector = new DataCollector();
+const dataCollector = new DataCollector(wikiOptions, wordsToClean);
 
-dataCollector.getAllCategories();
+dataCollector.getAllArtists()
+    .then(results => console.log(results))
+    .catch(e => console.log(e));
--- DataCollector/WikipediaClient.js
+++ DataCollector/WikipediaClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export class WikipediaClient {
-    getCategories(url, category) {
+    getArtists(url, category) {
         return axios.get(`${url}${category}&format=json&cmlimit=500`)
             .then(categories => categories.data)
             .catch(e => console.log(e));
