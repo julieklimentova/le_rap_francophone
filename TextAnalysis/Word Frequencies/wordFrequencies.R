05309a711851241444c8eba9305d69bb11eea1f7
Julie Klimentova <julie.klimentova@gmail.com>
2021-03-16 12:30:45 +0100

    TEXT ANALYSIS coocurrence

--- TextAnalysis/Word Frequencies/wordFrequencies.R
+++ TextAnalysis/Word Frequencies/wordFrequencies.R
@@ -1,6 +1,10 @@
 library(udpipe)
+library(textrank)
 library(tidyverse)
 library(lattice)
+library(igraph)
+library(ggraph)
+library(ggplot2)
 stopWords <- c(
   "a",
   "à",
@@ -158,7 +162,7 @@ stopWords <- c(
   "NA"
 )
 songsPath <- '../Experiments/metadata/songsMetadata.csv'
-songs <- readr::read_csv(songsPath)
+songs <- readr::read_csv(file = songsPath, locale = readr::locale(encoding = "latin1"))
 # ud_model <- udpipe_download_model(language = "french-partut")
 # ud_model <- udpipe_load_model(ud_model$file_model)
 names(songs)[3] <- 'doc_id'
@@ -209,3 +213,79 @@ mediaWords <- readr::read_csv(file = mediaWordsPath, locale = readr::locale(enco
 mediaWords$key <- factor(mediaWords$key, levels = rev(mediaWords$key))
 barchart(key ~ freq, data = head(mediaWords, 20), col = "cadetblue",
          main = "Most occurring media words", xlab = "Freq")
+
+
+# subset with media words 
+lemmas <- data.frame(annotation$lemma)
+
+nounsForCollocation <- subset(annotation, upos %in% c("NOUN")) 
+collocations <- keywords_collocation(x = nounsForCollocation, 
+                              term = "token", group = c("doc_id", "paragraph_id", "sentence_id"),
+                              ngram_max = 4)
+coocurrences <- cooccurrence(x = subset(annotation, upos %in% c("NOUN", "ADJ")), 
+                      term = "lemma", group = c("doc_id", "paragraph_id", "sentence_id"))
+## Co-occurrences: How frequent do words follow one another
+following <- cooccurrence(x = annotation$lemma, 
+                      relevant = annotation$upos %in% c("NOUN", "ADJ"))
+## Co-occurrences: How frequent do words follow one another even if we would skip 2 words in between
+followingSkipped <- cooccurrence(x = annotation$lemma, 
+                      relevant = annotation$upos %in% c("NOUN", "ADJ"), skipgram = 2)
+
+wordnetwork <- head(followingSkipped, 30)
+wordnetwork <- graph_from_data_frame(wordnetwork)
+ggraph(wordnetwork, layout = "fr") +
+  geom_edge_link(aes(width = cooc, edge_alpha = cooc), edge_colour = "pink") +
+  geom_node_text(aes(label = name), col = "darkgreen", size = 4) +
+  theme_graph(base_family = "Arial") +
+  theme(legend.position = "none") +
+  labs(title = "Cooccurrences within 3 words distance", subtitle = "Nouns & Adjective")
+
+# wordclouds
+
+stats <- textrank_keywords(annotation$lemma, 
+                           relevant = annotation$upos %in% c("NOUN", "ADJ"), 
+                           ngram_max = 8, sep = " ")
+statsKeywords <- subset(stats$keywords, ngram > 1 & freq >= 5)
+library(wordcloud)
+wordcloud(words = statsKeywords$keyword, freq = statsKeywords$freq)
+wordcloud(words = mediaWords$key, freq = mediaWords$freq)
+# | 
+#   'facebook'| 
+#   'twitter' |
+#   'poster' |
+#   'poste' |
+#   'taguer' |
+#   'tag' |
+#   'game' |
+#   'système'|
+#   'clique' |
+#   'cliquer' |
+#   'micro' |
+#   'ami' |
+#   'disque' |
+#   'dj' |
+#   'film' |
+#   'buzz' |
+#   'numéro' |
+#   'télé' |
+#   'midi' |
+#   'net' |
+#   'livre' |
+#   'single' |  
+#   'like' |
+#   'microphone' |
+#   'message' |
+#   'code' |
+#   'follow'|
+#   'percer' |
+#   'téléphone' |
+#   'cd' |
+#   'cro-mi' |
+#   'photo' |
+#   'internet' |
+#   'vidéo' |
+#   'écran' |
+#   'clip' |
+#   'phone' |
+#   'partager'
+
